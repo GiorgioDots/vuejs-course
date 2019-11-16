ec330af2eaa8b94d0e40a5c7a0379b4efc480cd6
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-11-16 14:46:16 +0100

    new module infos

---
+++ ExerciseComponents/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <heading></heading>
+    <hr />
+    <servers></servers>
+    <hr />
+    <info></info>
+  </div>
+</template>
+
+<script>
+import Heading from "./components/Heading.vue";
+import Servers from "./components/Servers.vue";
+import Info from "./components/Info.vue";
+export default {
+  components: {
+    heading: Heading,
+    servers: Servers,
+    info: Info
+  }
+};
+</script>
+
+<style>
+</style>
